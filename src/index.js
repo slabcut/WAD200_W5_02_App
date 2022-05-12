4734ebf0176c3410c2f9cf27a5f032d5e5e5482b
dave <dave@holycow.com.au>
2022-05-12 16:40:52 +1000

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import "./styles/index.css";
 
-import App from './App';
-
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <h1> Hello React </h1>
 );
