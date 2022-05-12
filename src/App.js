e747be452233bbb01af2208734bc54fe43e1c85a
slabcut <dave@holycow.com.au>
2022-05-12 16:43:42 +1000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+function App() {
+   return (
+      <>
+         <h1>Navigation</h1>
+         <h2>Cards</h2>
+         <h2>Footer</h2>
+      </>
+   );
 }
+export default App;
