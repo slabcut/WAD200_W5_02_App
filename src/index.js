297ec06bc570902a53b2bb300fde23e8c71f91a9
slabcut <dave@holycow.com.au>
2022-05-12 16:51:05 +1000

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from "./App.js";
+
 import "./styles/index.css";
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-    <h1> Hello React </h1>
+  <App />
 );
